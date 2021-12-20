890d7a5acda389ad2cf0e1e5de57462038d6a595
MestereoPa <antozik.ka@mail.ru>
2021-12-20 16:19:39 +0300

    Добаить класс хранитель.
    Было решено добавить класс хранитель для хранения элементов класа Route. Класс хранитель был почти полность скопирован с прошлой лабораторной работы т. к. в задачах небыло необходимости его написания. Так же внем будут описаны методы для выполнения задачь. То есть сортировка по номерам и поиск по номерам.

---
+++ LabaTp2.cpp
@@ -0,0 +1,28 @@
+﻿#include "Menu.h"
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int choise;
+	do
+	{
+		cout << "Выберите задание \n1 - работа с классом Route \n2 - работа с потоком \n0 - выход"<<endl;
+		cin >> choise;
+		if (choise == 1)
+		{
+			menuForRoute();
+		}
+		else
+		{
+			if (choise == 2)
+			{
+				menuForFile();
+			}
+			else
+			{
+				
+			}
+		}
+	}while (choise != 0);
+	return 0;
+}
+
